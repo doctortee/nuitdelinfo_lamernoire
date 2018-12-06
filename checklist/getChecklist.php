40d3ed3e4bb54454127afaeb566240094972bcce
DoctorTee <duvivieralexandre@free.fr>
2018-12-06 22:41:17 +0100

    encore

--- checklist/getChecklist.php
+++ checklist/getChecklist.php
@@ -1,7 +1,9 @@
 <?php header('Content-type: text/xml');
+header('Content-Type: text/html; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 //echo("It's Friday my Dudes");
 
-$fichier = fopen("./checklist.xml");
+$fichier = fopen("./checklist.xml", "r");
 if ($fichier) {
     while (($buffer = fgets($fichier, 4096)) !== false) {
         echo $buffer;
