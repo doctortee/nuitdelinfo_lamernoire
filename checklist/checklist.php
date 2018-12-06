64dd49c689d73feedb9a3911e78ef8c3be1afd5c
DoctorTee <duvivieralexandre@free.fr>
2018-12-06 23:57:52 +0100

    On revient en arrière

--- checklist/checklist.php
+++ checklist/checklist.php
@@ -6,6 +6,18 @@
 <body>
   <form action="http://sigmachine.ca/checklist/saveCheckList" method="post">
   <textarea cols="60" rows="10" wrap="soft" name="test" id="xmlcontent" hidden>
+    <?php
+    $fichier = fopen("./checklist.xml", "r");
+    if ($fichier) {
+        while (($buffer = fgets($fichier, 4096)) !== false) {
+            echo $buffer;
+        }
+        if (!feof($fichier)) {
+            echo "Erreur: fgets() a échoué\n";
+        }
+        fclose($fichier);
+    }
+    ?>
   </textarea>
   <br>
   <input type="submit" value="Valider" />
@@ -13,20 +25,7 @@
 </form>
 </body>
 <script>
-var text = '<?php
-$fichier = fopen("./checklist.xml", "r");
-if ($fichier) {
-    while (($buffer = fgets($fichier, 4096)) !== false) {
-        $buffer = preg_replace('#\R+#', '\\\n', $buffer);
-        echo $buffer."\\";
-    }
-    if (!feof($fichier)) {
-        echo "Erreur: fgets() a échoué\n";
-    }
-    fclose($fichier);
-}
-?>'
-//var text = document.getElementById("xmlcontent");
+var text = document.getElementById("xmlcontent").value;
 var parser = new DOMParser();
 var xmlDoc = parser.parseFromString(text, "text/xml")
 for (e in xmlDoc) {
