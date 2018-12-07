38e90784dfdf1e0f5633acb2e90e4fd68cd16944
ChloeDelahaye <32202855+ChloeDelahaye@users.noreply.github.com>
2018-12-07 03:20:39 +0100

    Authentification

---
+++ Authentification/login.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Nuit de l'info 2018</title>
+	<link rel="stylesheet" media="screen" href="authentication.css">
+	<script type="text/javascript" src="Authentication.js"></script> 
+</head>
+
+<body>
+
+	<div class="page-wrapper">
+		<header role="banner" >
+			<ul class="ulMenu">
+			<h1> Nuit de l'info 2018 </h1>
+			</ul>
+		</header>
+		
+		</br> 
+		</br>		
+<form name="authent" method="POST"  onsubmit="return verif();" action=Authentication.php>		
+		
+		<table class="tableau">					
+		
+			<tr>				
+				<td><label>Votre identifiant :</label></td>
+				<td vertical-align:text-top> <input type="text" name="user" id="user" style="width:250px;height:20px;text-align:center;font-size:15px;color:black;"> </td>
+				   		
+			</tr>
+			<tr>
+				<td><label>Code secret :</label></td>
+				<td> <input type="password" name="pass" id="password" style="width:250px;height:20px;text-align:center;;font-size:15px;color:black;"></td>
+			</tr>
+			<tr>
+				<td class="valider"><input type="submit" value="Valider" style="width:125px;height:25px"></td>
+			</tr>
+			
+            
+		</table>
+	
+	</div>
+	<div id="errMsg">
+	<?php  session_start();
+	if (isset($_SESSION['message']))
+{
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}?></div>
+</form>
+
+</div>
+</body>
+	
+</html>
